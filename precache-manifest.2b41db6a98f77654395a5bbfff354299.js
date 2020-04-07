adce807a20978f8877d83930ff471fe68a282536
dexstar1 <jonsaix852@gmail.com>
2020-04-07 17:29:57 +0100

    Updates

---
+++ precache-manifest.2b41db6a98f77654395a5bbfff354299.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ae3da59c84d3e51c7e94298dfc1fb1",
+    "url": "/mycity-app/index.html"
+  },
+  {
+    "revision": "1fb06876fbd57b863e89",
+    "url": "/mycity-app/static/css/2.a9d422b8.chunk.css"
+  },
+  {
+    "revision": "75819403fdd5fafa8c7c",
+    "url": "/mycity-app/static/css/main.e5135c8f.chunk.css"
+  },
+  {
+    "revision": "1fb06876fbd57b863e89",
+    "url": "/mycity-app/static/js/2.95469755.chunk.js"
+  },
+  {
+    "revision": "25f901a16418d77ba1d816ff1e88f9b0",
+    "url": "/mycity-app/static/js/2.95469755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75819403fdd5fafa8c7c",
+    "url": "/mycity-app/static/js/main.4b823444.chunk.js"
+  },
+  {
+    "revision": "5eda3d09a073c810a0dc",
+    "url": "/mycity-app/static/js/runtime-main.d22a54e2.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/mycity-app/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "/mycity-app/static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "fe349e8528c79623ea6795ddd110985a",
+    "url": "/mycity-app/static/media/10.fe349e85.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "/mycity-app/static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "/mycity-app/static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "/mycity-app/static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "/mycity-app/static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "/mycity-app/static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "2835396c930c8bf53ffd3beeedba7c41",
+    "url": "/mycity-app/static/media/7.2835396c.jpeg"
+  },
+  {
+    "revision": "10b775cc930b6986ec69eb9a4adef56a",
+    "url": "/mycity-app/static/media/8.10b775cc.jpeg"
+  },
+  {
+    "revision": "565ad4a387d2090e5d73f0c7b8de2793",
+    "url": "/mycity-app/static/media/9.565ad4a3.jpeg"
+  },
+  {
+    "revision": "77151175289435dd660b44f4cfb32f58",
+    "url": "/mycity-app/static/media/avatar.77151175.jpg"
+  }
+]);
